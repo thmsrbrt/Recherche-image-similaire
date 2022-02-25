796b9ad2a8d43a1cbdf4f5227413741e97ee9972
thmsrbrt <30445476+thmsrbrt@users.noreply.github.com>
2022-02-25 22:48:31 +0100

    update

--- src/Model/Projet.java
+++ src/Model/Projet.java
@@ -7,17 +7,22 @@ import fr.unistra.pelican.algorithms.io.ImageLoader;
 
 import java.io.File;
 import java.io.IOException;
-import java.sql.SQLException;
 import java.util.*;
 
 public class Projet {
-    private static TreeMap<Double, String> imageMap = new TreeMap<>();
-    private static String racine = "/Users/thomasrobert/Documents_IUT/Semestre4/Image/TD-image/Recherche img similaire/Images/";
-    private static BDD base;
+    private TreeMap<Double, String> imageMap;
+    private final String racine;
+    private final BDD base;
     private boolean methodeTypeHisto; //true = RGB ; false = HSV
 
-    public Projet(){
-
+    /**
+     * Constructeur Projet
+     */
+    public Projet() {
+        this.racine = "/Users/thomasrobert/Documents_IUT/Semestre4/Image/TD-image/Recherche_img_similaire/Images/";
+        this.base = new BDD();
+        this.imageMap = new TreeMap<>();
+        this.methodeTypeHisto = true;
     }
 
     /**
@@ -25,7 +30,7 @@ public class Projet {
      * @param img image d'entrée
      * @return une image d'ébruité avec le filtre médian
      */
-    public static Image filtreMedian(Image img) {
+    private Image filtreMedian(Image img) {
         int largeur = img.getXDim();
         int hauteur = img.getYDim();
         int nbDime = img.getBDim();
@@ -64,7 +69,7 @@ public class Projet {
      * @param image d'entrée
      * @return un tableau de TAILLE [3] [256]
      */
-    public static double[][] histogrammeRGB(Image image) {
+    private double[][] histogrammeRGB(Image image) {
         int largeur = image.getXDim();
         int hauteur = image.getYDim();
         int nbDime = image.getBDim();
@@ -93,7 +98,7 @@ public class Projet {
      * @param histo de taille [3][256]
      * @return histograme de taille [255][3]
      */
-    public static double[][] discretisationHisto(double[][] histo) {
+    private double[][] discretisationHisto(double[][] histo) {
         int tailleHisto = histo[0].length;
         int nbCanneaux = histo.length;
         double[][] histoDecretisation = new double[nbCanneaux][tailleHisto/10];
@@ -124,19 +129,119 @@ public class Projet {
      * @param nbPixel nombre pixel de l'image
      * @return un histogramme normalisé
      */
-    public static double[][] normalisationHisto(double[][] histo, int nbPixel) {
+    private double[][] normalisationHisto(double[][] histo, int nbPixel) {
         for (int i = 0; i < histo.length; i++)
             for (int j = 0; j < histo[i].length; j++)
                 histo[i][j] = histo[i][j] / nbPixel * 100;
-        //afficheHisto(histo);
         return histo;
+    }
+
+    /**
+     * Insert l'histogramme d'image dans la base de donnée
+     * @param name nom de l'image
+     * @param histo histogramme de l'image correspondant
+     */
+    private void insertHistoBDD(String name, double[][] histo) {
+        StringBuilder histoText = new StringBuilder();
+
+        for (double[] doubles : histo) {
+            for (double aDouble : doubles) {
+                histoText.append(aDouble).append(";");
+            }
+            histoText.append("\n");
+        }
+        base.insertToImageMoto(name, histoText.toString());
+    }
+
+    /**
+     * Traitement d'image et enregistrement en base de donnée
+     * @param dir Le dossier avec les images
+     */
+    public void init(String dir) {
+        String nomImage = "";
+        final File[] files = new File(racine + dir).listFiles();
+        assert files != null;
+
+        for(File item : files) {
+            nomImage = Objects.requireNonNull(item.getName());
+            if (!nomImage.contains(".jpg") ) {continue;}
+            Image image = ImageLoader.exec(racine + dir + nomImage);
+            double[][] val = normalisationHisto(discretisationHisto(histogrammeRGB(filtreMedian(image))), (image.getXDim() * image.getYDim()));
+            insertHistoBDD(nomImage, val);
+        }
+    }
+
+    /**
+     * Remplis la map avec les distances entre l'image de référence et les autres images.
+     */
+    public void similarite(String nomRef) {
+        final double[][] histoRef = histoStringToDouble(base.getImageMotoByName(nomRef));
+        String[] allHistosImage = base.getAllImage();
+        String[] allHistosImageName = allHistosImage[0].split(";");
+        String[] allHistosImageVal = allHistosImage[1].split("histo");
+
+        for (int i = 0; i < allHistosImage[0].split(";").length; i++) {
+            imageMap.put(distanceEntre2Histo(histoRef, histoStringToDouble(allHistosImageVal[i])), allHistosImageName[i]);
+        }
+    }
+
+    /**
+     * Prend un histogramme en format text et le retourne en tableau de double
+     * @param histoString histogramme en String
+     * @return histogramme en double[][]
+     */
+    private double[][] histoStringToDouble(String histoString){
+        double[][] histoDouble;
+        String[] histos = histoString.split("\n");
+        int nbHisto = histos.length;
+        int nbBarre = histos[0].split(";").length;
+        histoDouble = new double[nbHisto][nbBarre];
+        for (int i = 0; i < nbHisto; i++) {
+            String[] histo = histos[i].split(";");
+            for (int j = 0; j < nbBarre; j++) {
+                //System.out.println(histo[j]);
+                histoDouble[i][j] = Double.parseDouble(histo[j]);
+            }
+        }
+        return histoDouble;
+    }
+
+    /**
+     *
+     * @param histoR reference
+     * @param histoI à comparer
+     * @return distance entre les deux histos
+     */
+    private double distanceEntre2Histo (double[][] histoR, double[][] histoI) {
+        int nbCanal = histoI.length;
+        int nbBarre = histoI[0].length;
+        double distance = 0;
+
+        for (int i = 0; i < nbCanal; i++) {
+            for (int j = 0; j < nbBarre; j++) {
+                distance += Math.pow(histoR[i][j] - histoI[i][j], 2);
+            }
+        }
+        return Math.sqrt(distance);
+    }
+
+    public String[] getNbImageMap(int nbElement) {
+        if (nbElement > imageMap.size()) {
+            System.err.println("Le nombre d'image similaire demandé est supérieur à la base");
+            nbElement = imageMap.size();
+        }
+        String[] listNomImage = Arrays.toString(imageMap.values().toArray()).replace("[", "").replace("]", "").split(",");
+        String[] imagesName = new String[nbElement];
+        System.arraycopy(listNomImage, 1, imagesName, 0, nbElement);
+
+        return imagesName;
     }
 
     /**
      * Affichage d'un histogramme
      * @param histo quelconque
      */
-    public static void afficheHisto(double[][] histo) {
+    public void afficheHisto(double[][] histo) {
         if (histo.length == 3) {
             System.out.println("Affichage histogramme couleur");
             try {
@@ -157,127 +262,9 @@ public class Projet {
             System.out.println("taille d'histogramme non pris en charge");
     }
 
-    /**
-     * Remplis la map avec les distances entre l'image de référence et les autres images.
-     * @param imageRef reference
-     */
-    public static void Similarite(Image imageRef, String dir) {
-        final double[][] normalisationImageRefToHisto = normalisationImageToHisto(imageRef, null);
-        final File[] files = new File(racine + dir).listFiles();
-        assert files != null;
-        for(File item : files) {
-            String nomImage = Objects.requireNonNull(item.getName());
-            if (!nomImage.contains(".jpg") ) {continue;}
-            Image imageCompar = ImageLoader.exec(racine + dir + nomImage);
-            imageMap.put(similariteHisto(normalisationImageRefToHisto, normalisationImageToHisto(imageCompar, nomImage)), nomImage);
-        }
-    }
-
-    /**
-     *
-     * @param image quelconque en entrée
-     * @return un double[][] avec un histonormalisé
-     */
-    private static double[][] normalisationImageToHisto(Image image, String nomImage) {
-        double[][] val = normalisationHisto(discretisationHisto(histogrammeRGB(image)), (image.getXDim() * image.getYDim()));
-        insertHistoBDD(nomImage, val);
-        return  val;
-    }
-
-    /**
-     * Insert l'histogramme d'image dans la base de donnée
-     * @param name nom de l'image
-     * @param histo histogramme de l'image correspondant
-     */
-    private static void insertHistoBDD(String name, double[][] histo) {
-        StringBuilder histoText = new StringBuilder();
-
-        for (double[] doubles : histo) {
-            for (double aDouble : doubles) {
-                histoText.append(aDouble).append(";");
-            }
-            histoText.append("\n");
-        }
-
-        System.out.println(histoText);
-        System.out.println(name);
-        base.insertToImageMoto(name, histoText.toString());
-    }
-
-    /**
-     *
-     * @param histoR reference
-     * @param histoI à comparer
-     * @return distance entre les deux histos
-     */
-    private static double similariteHisto(double[][] histoR, double[][] histoI) {
-        int nbCanal = histoI.length;
-        int nbBarre = histoI[0].length;
-        double distance = 0;
-
-        for (int i = 0; i < nbCanal; i++) {
-            for (int j = 0; j < nbBarre; j++) {
-                distance += Math.pow(histoR[i][j] - histoI[i][j], 2);
-            }
-            //System.out.println("distance entre chaque canal " + distance);
-        }
-        return Math.sqrt(distance);
-    }
-
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        base = new BDD();
-        base.dropImageMotoTable();
-        base.creatImageMotoTable();
-        Image imageEiffel = ImageLoader.exec(racine + "img/eiffel.jpg");
-        //d'ébruiter l'image
-        imageEiffel = filtreMedian(imageEiffel);
-        //Viewer2D.exec(imageEiffel);
-
-        //double[][] histoRGB = histogrammeImage(imageEiffel);
-        //double[][] histRBGDiscretisation = discretisationHisto(histoRGB);
-        //double[][] histoNormalise = normalisationHisto(histRBGDiscretisation, (imageEiffel.getXDim()* imageEiffel.getYDim()));
-
-        //test comparaison 2 images
-        Image motoref = ImageLoader.exec(racine + "motos/001.jpg");
-        //double[][]  histoRef = normalisationImageToHisto(motoref);
-        //System.out.println("Teste 1 : "+similariteHisto(histoRef, normalisationImageToHisto(motore1)));
-        Similarite(motoref, "motos/");
-        //System.out.println(imageMap);
-        Image broad = ImageLoader.exec(racine + "broad/0001.png");
-
-
-
-        /*
-        imageMap.clear();
-        System.out.println(imageMap);
-        Similarite(broad, "broad/");
-        System.out.println(imageMap);
-
-         */
-
-    }
-    /*
-    private static void insertBDD() {
-        base.dropImageMotoTable();
-        base.creatImageMotoTable();
-        Set<Double> keys = imageMap.keySet();
-        for(Double key: keys){
-            //base.insertTable(imageMap.get(key), key);
-        }
-    }
-     */
-
-/*
-    private static ArrayList[][] getImgRessemblance() throws SQLException, ClassNotFoundException {
-        return base.resemblance(10);
-    }
-
- */
-
-
-
-    public static String getRacine() {
-        return racine;
+    public String getRacine() {
+        return this.racine;
     }
 
 }
+
