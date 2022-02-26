21090057343a80b53d926925822191fcaed12438
thmsrbrt <30445476+thmsrbrt@users.noreply.github.com>
2022-02-26 09:38:25 +0100

    add controller attribut

--- src/Appli.java
+++ src/Appli.java
@@ -1,13 +1,18 @@
-import Model.Projet;
+import Controller.ControllerNbImageOut;
+import Controller.ControllerSearch;
+import Controller.ControllerTypeHisto;
+import Model.ResearchPicture;
 import Vue.Vue;
 
 public class Appli {
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                Projet modele = new Projet();
-                Vue vue = new Vue();
-                //ControlButton controlButton = new ControlButton(modele, vue);
+                ResearchPicture model = new ResearchPicture();
+                Vue vue = new Vue(model);
+                ControllerSearch controllerSearch = new ControllerSearch(model, vue);
+                ControllerTypeHisto controllerTypeHisto = new ControllerTypeHisto(model, vue);
+                ControllerNbImageOut controllerNbImageOut = new ControllerNbImageOut(model, vue);
             }
         });
     }
