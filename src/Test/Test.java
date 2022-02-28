8c179a57f500fde07635cf4ea1d31543801c0713
thmsrbrt <30445476+thmsrbrt@users.noreply.github.com>
2022-02-28 15:33:13 +0100

    fix

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -9,12 +9,15 @@ public class Test {
     public static void main(String[] args) {
         ResearchPicture researchPicture = new ResearchPicture();
         BDD base = new BDD();
-        base.dropImageTable();
-        base.creatImageTable();
-        researchPicture.init("motos/");
+        //base.dropImageTable();
+        //base.creatImageTable();
+        //researchPicture.init("motos/");
         //base.getImageByName("002.jpg");
         researchPicture.similarite("002.jpg");
-
+        researchPicture.setMethodeTypeHisto(false);
+        researchPicture.similarite("002.jpg");
+        //
+        //[ 008.jpg,  003.jpg,  064.jpg,  007.jpg,  016.jpg,  005.jpg,  019.jpg,  006.jpg,  267.jpg,  260.jpg]
         System.out.println(Arrays.toString(researchPicture.getNbImageMap(10)));
     }
 }
